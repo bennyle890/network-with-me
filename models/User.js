0a5d9447f428c181f281f50c450de70702e17b84
Benny <benny.le890@gmail.com>
2022-11-15 22:54:13 -0500

    maybe finished user schema?

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: 'Username is required',
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email is required',
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            ref: 'Thought',
+            type: Schema.Types.ObjectId
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
