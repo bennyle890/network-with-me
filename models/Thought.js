944f1491741a10538f22ea0a3dcb5c052c779794
Benny <benny.le890@gmail.com>
2022-11-15 23:02:01 -0500

    finished thought schema?

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Please fill out text",
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat (createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Username required'
+        },
+        reactions: [ReactionSchema]
+    }
+);
+
+const ReactionSchema = new Schema({
+    
+})
