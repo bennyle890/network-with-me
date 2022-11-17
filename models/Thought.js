282f44eefa173aa3a01d0e8e9cc13b600f5a76f9
Benny <benny.le890@gmail.com>
2022-11-17 14:47:59 -0500

    put reactionSchema before thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: "Please fill out text",
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat (createdAtVal)
-        },
-        username: {
-            type: String,
-            required: 'Username required'
-        },
-        reactions: [ReactionSchema]
-    }
-);
-
 const ReactionSchema = new Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
@@ -42,6 +21,27 @@ const ReactionSchema = new Schema({
         get: createdAtVal => dateFormat (createdAtVal)
     }
 });
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Please fill out text",
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat (createdAtVal)
+        },
+        username: {
+            type: String,
+            required: 'Username required'
+        },
+        reactions: [ReactionSchema]
+    }
+);
 
 ThoughtSchema.virtual('reactionCount').get(function() {
     return this.reaction.length;
